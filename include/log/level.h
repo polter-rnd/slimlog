8ac91961f28fdef6f413b6a6fa65f9ca9dfcc20b
Pavel Artsishevsky <polter.rnd@gmail.com>
2024-05-04 22:06:34 +0200

    Add documentation for level.h
    Signed-off-by: Pavel Artsishevsky <polter.rnd@gmail.com>

--- include/log/level.h
+++ include/log/level.h
@@ -1,3 +1,8 @@
+/**
+ * @file level.h
+ * @brief Contains definition of Level and LevelDriver classes.
+ */
+
 #pragma once
 
 #include "policy.h"
@@ -6,30 +11,48 @@
 
 namespace PlainCloud::Log {
 
-enum class Level { Fatal, Error, Warning, Info, Debug, Trace };
-
-template<typename ThreadingPolicy>
-class LevelDriver { };
+/**
+ * @brief Logging level.
+ *
+ * Specifies log event severity (e.g. `Debug`, `Info`, `Warning`)
+ */
+enum class Level {
+    Fatal, ///< Very severe error events that will presumably lead the application to abort.
+    Error, ///< Error events that might still allow the application to continue running.
+    Warning, ///< Potentially harmful situations.
+    Info, ///< Messages that highlight the progress of the application at coarse-grained level.
+    Debug, ///< Fine-grained informational events that are most useful to debug an application.
+    Trace ///< Used to "trace" entry and exiting of methods.
+};
 
 /**
- * @brief A single thread log level driver.
+ * @brief Basic log level driver class.
  *
- * Handles log level access without any synchronization.
+ * Used to handle thread-safe manipulation of logging level field.
+ *
+ * @tparam ThreadingPolicy Threading policy used for operating over log level
+ *                         (e.g. SingleThreadedPolicy or MultiThreadedPolicy).
  *
  * @note Class doesn't have a virtual destructor
  *       as the intended usage scenario is to
  *       use it as a private base class explicitly
  *       moving access functions to public part of a base class.
  */
+template<typename ThreadingPolicy>
+class LevelDriver { };
+
+/**
+ * @brief Single-threaded log level driver.
+ *
+ * Handles log level access without any synchronization.
+ */
 template<>
-class LevelDriver<SingleThreadedPolicy> {
+class LevelDriver<SingleThreadedPolicy> final {
 public:
     explicit LevelDriver(Level level) noexcept
         : m_level{level}
     {
     }
-
-    // No virtual destructor as the data is trivial.
 
     /**
      * @brief Get currently enabled log level.
@@ -44,7 +67,7 @@ public:
     /**
      * @brief Set log level for logger.
      *
-     * @param level  New log level for logger.
+     * @param level New log level for logger.
      */
     auto operator=(Level level) noexcept -> auto&
     {
@@ -56,38 +79,23 @@ private:
     Level m_level;
 };
 
-//
-// LevelDriverMT
-//
-
 /**
- * @brief A multi thread log level driver.
+ * @brief Multi-threaded log level driver.
  *
  * Handles log level access with atomic.
- *
- * @note Class doesn't have a virtual destructor
- *       as the intended usage scenario is to
- *       use it as a private base class explicitly
- *       moving access functions to public part of a base class.
- *
- * @tparam Load_MO  Memory order for loading value.
- * @tparam Store_MO  Memory order for storing new value.
  */
-
 template<
     typename Mutex,
     typename ReadLock,
     typename WriteLock,
     std::memory_order LoadOrder,
     std::memory_order StoreOrder>
-class LevelDriver<MultiThreadedPolicy<Mutex, ReadLock, WriteLock, LoadOrder, StoreOrder>> {
+class LevelDriver<MultiThreadedPolicy<Mutex, ReadLock, WriteLock, LoadOrder, StoreOrder>> final {
 public:
     explicit LevelDriver(Level level) noexcept
         : m_level{level}
     {
     }
-
-    // No virtual destructor as the data is trivial.
 
     /**
      * @brief Get currently enabled log level.
