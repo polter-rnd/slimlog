d5e929aa89801c74a79b6ccac6938b3358862b12
Pavel Artsishevsky <polter.rnd@gmail.com>
2024-05-04 22:47:26 +0200

    Add documentation for policy.h
    Signed-off-by: Pavel Artsishevsky <polter.rnd@gmail.com>

--- include/log/policy.h
+++ include/log/policy.h
@@ -1,3 +1,8 @@
+/**
+ * @file policy.h
+ * @brief Contains definition of SingleThreadedPolicy and MultiThreadedPolicy classes.
+ */
+
 #pragma once
 
 #include <atomic>
@@ -6,8 +11,24 @@
 
 namespace PlainCloud::Log {
 
+/**
+ * @brief Single-threaded policy.
+ *
+ * Used to handle data manipulation without any locks or atomics.
+ */
 struct SingleThreadedPolicy { };
 
+/**
+ * @brief Multi-threaded policy.
+ *
+ * Used to handle thread-safe data manipulation at cost of possible locking.
+ *
+ * @tparam Mutex Mutex type used for locking.
+ * @tparam ReadLock Lock type used for read-only access.
+ * @tparam WriteLock Lock type used for write access.
+ * @tparam LoadOrder Memory order for loading value.
+ * @tparam StoreOrder Memory order for storing new value.
+ */
 template<
     typename Mutex = std::shared_mutex,
     typename ReadLock = std::shared_lock<Mutex>,
