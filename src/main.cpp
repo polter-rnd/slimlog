5046177c6f6f1665e8a2f43e894e3b395887d160
Pavel Artsishevsky <polter.rnd@gmail.com>
2024-06-21 11:15:30 +0200

    Add benchmark
    Signed-off-by: Pavel Artsishevsky <polter.rnd@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,57 +2,167 @@
 #include "log/level.h"
 #include "log/logger.h"
 #include "log/policy.h"
+#include "log/sinks/dummy_sink.h"
 #include "log/sinks/ostream_sink.h"
 #include "util.h"
 
+#include <algorithm>
 #include <exception>
 #include <iostream>
 #include <memory>
+#include <random>
 #include <sstream>
 #include <string_view>
 #include <utility>
 
-static constexpr auto SomeConstant = 1234;
+class null_out_buf : public std::basic_streambuf<wchar_t> {
+public:
+    virtual std::streamsize xsputn(const char*, std::streamsize n)
+    {
+        return n;
+    }
+    virtual int overflow(int)
+    {
+        return 1;
+    }
+};
+
+class null_out_stream : public std::basic_ostream<wchar_t> {
+public:
+    null_out_stream()
+        : std::basic_ostream<wchar_t>(&buf)
+    {
+    }
+
+private:
+    null_out_buf buf;
+};
+
+template<typename Func>
+auto benchmark(Func test_func)
+{
+    const auto start = std::chrono::steady_clock::now();
+    test_func();
+    const auto stop = std::chrono::steady_clock::now();
+    const auto secs = std::chrono::duration<double>(stop - start);
+    return secs.count();
+}
+
+template<typename T>
+T gen_random(const int len)
+{
+    static const wchar_t alphanum[] = L"0123456789"
+                                      L"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                      L"abcdefghijklmnopqrstuvwxyz"
+                                      L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+                                      L"абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+                                      L" \t";
+    T tmp_s;
+    tmp_s.reserve(len);
+
+    // Create a random number generator
+    static std::random_device rd;
+    static std::mt19937 generator(rd());
+
+    // Create a distribution to uniformly select from all
+    // characters
+    static std::uniform_int_distribution<> distribution(0, sizeof(alphanum) / sizeof(wchar_t) - 1);
+
+    for (int i = 0; i < len; ++i) {
+        tmp_s += alphanum[distribution(generator)];
+    }
+
+    return tmp_s;
+}
+
+double percentile(std::vector<double>& vectorIn, double percent)
+{
+    auto nth = vectorIn.begin() + (percent * vectorIn.size()) / 100;
+    std::nth_element(vectorIn.begin(), nth, vectorIn.end());
+    return *nth;
+}
+
+void do_test()
+{
+    namespace Log = PlainCloud::Log;
+
+    null_out_stream devnull;
+
+    srand((unsigned)time(NULL) * getpid());
+
+    auto log_root = std::make_shared<Log::Logger<std::wstring_view>>(L"main");
+
+    for (int i = 0; i < 50; i++) {
+        // std::cout << "testing root syncs: " << i << "\n";
+        log_root->add_sink<Log::OStreamSink>(
+            devnull,
+            L"(%t) [%l] %F|%L: %m",
+            std::make_pair(Log::Level::Trace, gen_random<std::wstring>(5)),
+            std::make_pair(Log::Level::Debug, gen_random<std::wstring>(5)),
+            std::make_pair(Log::Level::Warning, gen_random<std::wstring>(5)),
+            std::make_pair(Log::Level::Error, gen_random<std::wstring>(5)),
+            std::make_pair(Log::Level::Fatal, gen_random<std::wstring>(5)));
+
+        /*for (int j = 0; j < 1024; j++) {
+            log_root->info(L"This is random message {}: {}", j, gen_random<std::wstring>(512));
+        }*/
+    }
+
+    std::vector<std::shared_ptr<Log::Logger<std::wstring_view>>> log_children;
+    for (int i = 0; i < 10; i++) {
+        // std::cout << "adding children: " << i << "\n";
+        auto child = std::make_shared<Log::Logger<std::wstring_view>>(
+            L"child_" + std::to_wstring(i), log_root);
+        log_children.push_back(child);
+
+        for (int j = 0; j < 100; j++) {
+            child->add_sink<Log::OStreamSink>(
+                devnull, L">sink_" + std::to_wstring(j) + L"< (%t) [%l] %F|%L: %m");
+        }
+        /*for (int j = 0; j < 1024; j++) {
+            child->info(L"This is random message {}: {}", j, gen_random<std::wstring>(512));
+        }*/
+    }
+
+    std::vector<double> results;
+
+    int k = 0;
+    for (const auto& child : log_children) {
+        std::vector<std::shared_ptr<Log::Logger<std::wstring_view>>> children2;
+        std::shared_ptr<Log::Logger<std::wstring_view>> child2 = child;
+        for (int i = 0; i < 100; i++) {
+            child2 = std::make_shared<Log::Logger<std::wstring_view>>(
+                L"child_new_" + std::to_wstring(i), child2);
+            children2.push_back(child2);
+
+            for (int j = 0; j < 10; j++) {
+                child2->add_sink<Log::OStreamSink>(
+                    devnull, L">new_sink_" + std::to_wstring(j) + L"< (%t) [%l] %F|%L: %m");
+            }
+        }
+        auto res = benchmark([&child2]() {
+            for (int j = 0; j < 1024; j++) {
+                child2->info(
+                    L"This is random message {}: {}", j, gen_random<std::wstring>(256 + j));
+            }
+        });
+        std::cout << "[" << k++ << "] message emitting: " << res << "\n";
+        results.push_back(res);
+    }
+
+    std::cout << "90 perc: " << percentile(results, 90) << "\n";
+    std::cout << "median: " << percentile(results, 50) << "\n";
+}
 
 auto main(int /*argc*/, char* /*argv*/[]) -> int
 {
-    namespace Log = PlainCloud::Log;
     namespace Util = PlainCloud::Util;
 
     try {
         // replace the C++ global locale and the "C" locale with the user-preferred locale
         const Util::ScopedGlobalLocale myloc("");
-
-#ifdef ENABLE_FMTLIB
-        const std::basic_stringstream<char8_t> mystream;
-        Log::Logger log19{u8"uchar8 log"};
-        log19.add_sink<Log::OStreamSink>(mystream, u8"(%t) [%l] %F|%L: %m");
-        log19.info(u8"hello from u8!");
-        std::cout << std::string_view(
-            // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-            reinterpret_cast<const char*>(mystream.view().data()),
-            mystream.view().size())
-                  << "\n";
-#endif
-
-        auto log_root = std::make_shared<Log::Logger<std::string_view>>("root");
-
-        auto root_sink = log_root->add_sink<Log::OStreamSink>(std::cout, "(%t) [%l] %F|%L: %m");
-        log_root->message(Log::Level::Info, "Root!!!");
-
-        Log::Logger log_child("kek_child", Log::Level::Info);
-        log_child.add_sink(root_sink);
-        log_child.info("Root sink enabled: {}", "Helloo!!");
-        log_child.info("Root sink enabled: {}", "Helloo!!");
-        log_child.info("Root sink enabled: {}", "Helloo!!");
-        log_child.info("Root sink enabled: {}", "Helloo!!");
-        log_child.info("Root sink enabled: {}", "Helloo!!");
-        log_child.info("Root sink enabled: {}", "Helloo!!");
-        log_child.info("Root sink enabled: {}", "Helloo!!");
-        log_child.info("Root sink enabled: {}", "Helloo!!");
-
-        log_root->set_sink_enabled(root_sink, false);
-        log_child.info("Root sink disabled!");
+        const double t1 = benchmark(do_test);
+        std::cout << "TOTAL RESULT: " << t1 << std::endl;
     } catch (const std::exception& e) {
         std::cerr << "Exception: " << e.what() << '\n';
         return 1;
