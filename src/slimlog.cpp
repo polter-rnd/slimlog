fd03916e98ddafbfe2eed4c5ebb14d595e7c4b75
Pavel Artsishevsky <polter.rnd@gmail.com>
2024-11-20 11:02:23 +0100

    Add more template specializations to slimlog.cpp
    Signed-off-by: Pavel Artsishevsky <polter.rnd@gmail.com>

--- src/slimlog.cpp
+++ src/slimlog.cpp
@@ -15,12 +15,45 @@
 #include <string_view>
 
 namespace SlimLog {
+// char
 template class SinkDriver<Logger<std::string_view>, SingleThreadedPolicy>;
 template class SinkDriver<Logger<std::string_view>, MultiThreadedPolicy>;
-
 template class Sink<Logger<std::string_view>>;
-
+template class RecordStringView<char>;
 template class Pattern<char>;
 
-template class RecordStringView<char>;
+// wchar_t
+template class SinkDriver<Logger<std::wstring_view>, SingleThreadedPolicy>;
+template class SinkDriver<Logger<std::wstring_view>, MultiThreadedPolicy>;
+template class Sink<Logger<std::wstring_view>>;
+template class RecordStringView<wchar_t>;
+template class Pattern<wchar_t>;
+
+// char8_t (std::mbrtoc8() is supported only for newer versions of glibc++ and libc++)
+#if defined(__cpp_char8_t)
+#if defined(_GLIBCXX_USE_UCHAR_C8RTOMB_MBRTOC8_CXX20)                                              \
+    or defined(_LIBCPP_VERSION) and !defined(_LIBCPP_HAS_NO_C8RTOMB_MBRTOC8)
+template class SinkDriver<Logger<std::u8string_view>, SingleThreadedPolicy>;
+template class SinkDriver<Logger<std::u8string_view>, MultiThreadedPolicy>;
+template class Sink<Logger<std::u8string_view>>;
+template class RecordStringView<char8_t>;
+template class Pattern<char8_t>;
+#endif
+#endif
+
+// char16_t
+#if defined(__cpp_unicode_characters)
+template class SinkDriver<Logger<std::u16string_view>, SingleThreadedPolicy>;
+template class SinkDriver<Logger<std::u16string_view>, MultiThreadedPolicy>;
+template class Sink<Logger<std::u16string_view>>;
+template class RecordStringView<char16_t>;
+template class Pattern<char16_t>;
+
+// char32_t
+template class SinkDriver<Logger<std::u32string_view>, SingleThreadedPolicy>;
+template class SinkDriver<Logger<std::u32string_view>, MultiThreadedPolicy>;
+template class Sink<Logger<std::u32string_view>>;
+template class RecordStringView<char32_t>;
+template class Pattern<char32_t>;
+#endif
 } // namespace SlimLog
