d7e3d2dcad3b77efb821e233d3359cf60f04ed37
Pavel Artsishevsky <polter.rnd@gmail.com>
2024-12-01 02:41:23 +0100

    Add support for fopen_s() on Windows
    Signed-off-by: Pavel Artsishevsky <polter.rnd@gmail.com>

--- include/slimlog/sinks/file_sink-inl.h
+++ include/slimlog/sinks/file_sink-inl.h
@@ -17,15 +17,69 @@
 #include <cerrno>
 #include <cstdio>
 #include <memory>
+#include <string>
 #include <string_view>
 #include <system_error>
+#include <variant>
 
 namespace SlimLog {
+
+/** @cond */
+namespace Detail {
+
+namespace Fallback {
+template<typename... Args>
+inline auto fopen_s(Args... /*unused*/)
+{
+    return std::monostate{};
+};
+} // namespace Fallback
+
+struct FOpen {
+    using FilePtr = std::unique_ptr<FILE, int (*)(FILE*)>;
+
+    FOpen() = default;
+    ~FOpen() = default;
+
+    FOpen(const FOpen&) = delete;
+    FOpen(FOpen&&) = delete;
+    auto operator=(const FOpen&) -> FOpen& = delete;
+    auto operator=(FOpen&&) noexcept -> FOpen& = delete;
+
+    auto open(const char* filename, const char* mode) -> FilePtr
+    {
+        using namespace Fallback;
+        using namespace std;
+        m_filename = filename;
+        m_mode = mode;
+        return handle(fopen_s(&m_fp, m_filename, m_mode));
+    }
+
+protected:
+    auto handle(int /*unused*/) -> FilePtr
+    {
+        return {m_fp, std::fclose};
+    }
+
+    template<typename T = std::monostate>
+    auto handle(T /*unused*/) -> FilePtr
+    {
+        return {std::fopen(m_filename, m_mode), std::fclose};
+    }
+
+private:
+    FILE* m_fp = nullptr;
+    const char* m_filename = nullptr;
+    const char* m_mode = nullptr;
+};
+
+} // namespace Detail
+/** @endcond */
 
 template<typename Logger>
 auto FileSink<Logger>::open(std::string_view filename) -> void
 {
-    m_fp = {std::fopen(filename.data(), "w+"), std::fclose};
+    m_fp = Detail::FOpen().open(std::string(filename).c_str(), "w+");
     if (!m_fp) {
         throw std::system_error({errno, std::system_category()}, "Error opening log file");
     }
