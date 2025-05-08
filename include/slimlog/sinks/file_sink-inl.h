c51f7daa849f19e426b36c46b98e2a72e60a78f9
Pavel Artsishevsky <polter.rnd@gmail.com>
2025-05-09 01:06:22 +0200

    Write BOM to UTF-32/UTF-16 files. Test all supported types
    Signed-off-by: Pavel Artsishevsky <polter.rnd@gmail.com>

--- include/slimlog/sinks/file_sink-inl.h
+++ include/slimlog/sinks/file_sink-inl.h
@@ -10,6 +10,8 @@
 // NOLINTNEXTLINE(misc-header-include-cycle)
 #include "slimlog/sinks/file_sink.h" // IWYU pragma: associated
 
+#include <array>
+#include <bit>
 #include <cerrno>
 #include <cwchar>
 #include <string>
@@ -30,6 +32,45 @@ auto FileSink<String, Char, BufferSize, Allocator>::open(std::string_view filena
     if (!m_fp) {
         throw std::system_error({errno, std::system_category()}, "Error opening log file");
     }
+
+    if (std::fseek(m_fp.get(), 0, SEEK_END) != 0) {
+        throw std::system_error({errno, std::system_category()}, "Error seeking log file");
+    }
+
+    const auto size = std::ftell(m_fp.get());
+    if (size == -1) {
+        throw std::system_error({errno, std::system_category()}, "Error getting log file size");
+    }
+
+    // Write BOM only if the file is empty
+    if (size == 0 && !write_bom()) {
+        throw std::system_error({errno, std::system_category()}, "Error writing BOM to log file");
+    }
+}
+
+template<typename String, typename Char, std::size_t BufferSize, typename Allocator>
+auto FileSink<String, Char, BufferSize, Allocator>::write_bom() -> bool
+{
+    if constexpr (
+        std::is_same_v<Char, char16_t> || (std::is_same_v<Char, wchar_t> && sizeof(wchar_t) == 2)) {
+        // UTF-16 BOM (or 2-byte wchar_t)
+        static constexpr std::array<unsigned char, 2> LeBom = {0xFF, 0xFE}; // UTF-16LE
+        static constexpr std::array<unsigned char, 2> BeBom = {0xFE, 0xFF}; // UTF-16BE
+        const auto& bom = (std::endian::native == std::endian::little) ? LeBom : BeBom;
+        return std::fwrite(bom.data(), bom.size(), 1, m_fp.get()) == 2;
+    } else if constexpr (
+        std::is_same_v<Char, char32_t> || (std::is_same_v<Char, wchar_t> && sizeof(wchar_t) == 4)) {
+        // UTF-32 BOM (or 4-byte wchar_t)
+        static constexpr std::array<unsigned char, 4> LeBom = {0xFF, 0xFE, 0x00, 0x00}; // UTF-32LE
+        static constexpr std::array<unsigned char, 4> BeBom = {0x00, 0x00, 0xFE, 0xFF}; // UTF-32BE
+        const auto& bom = (std::endian::native == std::endian::little) ? LeBom : BeBom;
+        return std::fwrite(bom.data(), bom.size(), 1, m_fp.get()) == 4;
+    } else if constexpr (std::is_same_v<Char, char8_t> || std::is_same_v<Char, char>) {
+        // UTF-8 BOM
+        static constexpr std::array<unsigned char, 3> Bom = {0xEF, 0xBB, 0xBF};
+        return std::fwrite(Bom.data(), Bom.size(), 1, m_fp.get()) == 3;
+    }
+    return true;
 }
 
 template<typename String, typename Char, std::size_t BufferSize, typename Allocator>
