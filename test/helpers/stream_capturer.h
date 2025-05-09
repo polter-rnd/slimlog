c0f474cdf01b05cc0d876c85d37559b2b452f338
Pavel Artsishevsky <polter.rnd@gmail.com>
2025-05-09 12:59:08 +0200

    Tests: rename OutputCapturer to StreamCapturer and use it as a stream
    Signed-off-by: Pavel Artsishevsky <polter.rnd@gmail.com>

---
+++ test/helpers/stream_capturer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iterator>
+#include <sstream>
+#include <string>
+
+template<typename Char>
+class StreamCapturer : public std::basic_stringstream<Char> {
+public:
+    using std::basic_stringstream<Char>::basic_stringstream;
+
+    explicit StreamCapturer(std::basic_ostream<Char>& stream)
+        : m_stream(&stream)
+        , m_buf(m_stream->rdbuf(this->rdbuf()))
+    {
+    }
+
+    auto read() -> std::basic_string<Char>
+    {
+        sync();
+        return {std::istreambuf_iterator<Char>(*this), std::istreambuf_iterator<Char>()};
+    }
+
+    ~StreamCapturer() noexcept override
+    {
+        if (m_stream) {
+            m_stream->rdbuf(m_buf);
+        }
+    }
+
+private:
+    std::basic_ostream<Char>* m_stream = nullptr;
+    std::basic_streambuf<Char>* m_buf = nullptr;
+};
