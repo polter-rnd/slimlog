cd73751edc3c6d34a645d13bc1c54e94c190a3ab
Pavel Artsishevsky <polter.rnd@gmail.com>
2025-06-28 22:10:16 +0200

    Pattern: fix owning log level name strings
    Signed-off-by: Pavel Artsishevsky <polter.rnd@gmail.com>

--- include/slimlog/pattern.h
+++ include/slimlog/pattern.h
@@ -73,19 +73,19 @@ public:
         auto set(Level level, StringViewType name) -> void;
 
     private:
-        static constexpr std::array<Char, 6> DefaultTrace{'T', 'R', 'A', 'C', 'E', '\0'};
-        static constexpr std::array<Char, 6> DefaultDebug{'D', 'E', 'B', 'U', 'G', '\0'};
-        static constexpr std::array<Char, 5> DefaultInfo{'I', 'N', 'F', 'O', '\0'};
-        static constexpr std::array<Char, 5> DefaultWarning{'W', 'A', 'R', 'N', '\0'};
-        static constexpr std::array<Char, 6> DefaultError{'E', 'R', 'R', 'O', 'R', '\0'};
-        static constexpr std::array<Char, 6> DefaultFatal{'F', 'A', 'T', 'A', 'L', '\0'};
+        std::basic_string<Char> m_trace = {'T', 'R', 'A', 'C', 'E'};
+        std::basic_string<Char> m_debug = {'D', 'E', 'B', 'U', 'G'};
+        std::basic_string<Char> m_info = {'I', 'N', 'F', 'O'};
+        std::basic_string<Char> m_warning = {'W', 'A', 'R', 'N'};
+        std::basic_string<Char> m_error = {'E', 'R', 'R', 'O', 'R'};
+        std::basic_string<Char> m_fatal = {'F', 'A', 'T', 'A', 'L'};
 
-        RecordStringView<Char> m_trace{DefaultTrace.data()}; ///< Trace level
-        RecordStringView<Char> m_debug{DefaultDebug.data()}; ///< Debug level
-        RecordStringView<Char> m_info{DefaultInfo.data()}; ///< Info level
-        RecordStringView<Char> m_warning{DefaultWarning.data()}; ///< Warning level
-        RecordStringView<Char> m_error{DefaultError.data()}; ///< Error level
-        RecordStringView<Char> m_fatal{DefaultFatal.data()}; ///< Fatal level
+        RecordStringView<Char> m_trace_sv = m_trace; ///< Trace level
+        RecordStringView<Char> m_debug_sv = m_debug; ///< Debug level
+        RecordStringView<Char> m_info_sv = m_info; ///< Info level
+        RecordStringView<Char> m_warning_sv = m_warning; ///< Warning level
+        RecordStringView<Char> m_error_sv = m_error; ///< Error level
+        RecordStringView<Char> m_fatal_sv = m_fatal; ///< Fatal level
     };
 
     /**
