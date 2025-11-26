520290345837d1f3004cb84d543000cff95a6eef
Pavel Artsishevsky <polter.rnd@gmail.com>
2025-11-26 14:20:46 +0100

    Location: use Detail::source_basename() instead of lambda
    Otherwise clang-tidy 18 goes mad with consteval lambda.
    Also wrap other	builtins to SLIMLOG_SOURCE_... defines.

--- include/slimlog/location.h
+++ include/slimlog/location.h
@@ -7,6 +7,33 @@
 
 namespace SlimLog {
 
+// MSVC doesn't have __has_builtin, source_location supported from MSVC 16.6 (MSVC-PR-229114)
+#if (!defined(__has_builtin) and !defined(_MSC_VER)) or (defined(_MSC_VER) and _MSC_VER < 1926)
+#define SLIMLOG_SOURCE_FILE "unknown"
+#define SLIMLOG_SOURCE_FUNCTION "unknown"
+#define SLIMLOG_SOURCE_LINE -1
+#else
+#define SLIMLOG_SOURCE_FILE __builtin_FILE()
+#define SLIMLOG_SOURCE_FUNCTION __builtin_FUNCTION()
+#define SLIMLOG_SOURCE_LINE __builtin_LINE()
+#endif
+
+/** @cond */
+namespace Detail {
+static consteval auto source_basename(const char* path = SLIMLOG_SOURCE_FILE) -> const char*
+{
+    const char* filename = path;
+    while (*path != '\0') {
+        if (*path == '\\' || *path == '/') {
+            filename = path + 1;
+        }
+        ++path;
+    }
+    return filename;
+}
+} // namespace Detail
+/** @endcond */
+
 /**
  * @brief Represents a specific location in the source code.
  *
@@ -23,30 +50,9 @@ public:
      * @return A Location object representing the current source location.
      */
     [[nodiscard]] static constexpr auto current(
-#ifndef __has_builtin
-/** @cond */
-#define __has_builtin(__x) 0
-/** @endcond */
-#endif
-#if __has_builtin(__builtin_FILE) and __has_builtin(__builtin_FUNCTION)                            \
-        and __has_builtin(__builtin_LINE)                                                          \
-    or defined(_MSC_VER) and _MSC_VER >= 1926 // starting from MSVC 16.6 (MSVC-PR-229114)
-        const char* file = [](const char* path = __builtin_FILE()) consteval -> auto {
-            const char* file = path;
-            while (*path != '\0') {
-                if (*path == '\\' || *path == '/') {
-                    file = path + 1;
-                }
-                ++path;
-            }
-            return file;
-        }(),
-        const char* function = __builtin_FUNCTION(),
-        int line = __builtin_LINE()
-#else
-        const char* file = "unknown", const char* function = "unknown", int line = -1
-#endif
-            ) noexcept
+        const char* file = Detail::source_basename(),
+        const char* function = SLIMLOG_SOURCE_FUNCTION,
+        int line = SLIMLOG_SOURCE_LINE) noexcept
     {
         Location loc{};
         loc.m_file = file;
